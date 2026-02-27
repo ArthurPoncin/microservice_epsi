948d70067f24bf7bbceb64d3d25e5e9fee0b8bed
Arthur <poncin.arthur@proton.me>
2026-02-27 16:44:41 +0100

    Fix: Docker compose, tests

--- payment-service/src/main/java/com/example/payment_service/repository/RegistrationRepository.java
+++ payment-service/src/main/java/com/example/payment_service/repository/RegistrationRepository.java
@@ -1,14 +1,16 @@
-package com.example.MicroService_Event.repository;
+package com.example.payment_service.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.MicroService_Event.model.Registration;
+import com.example.payment_service.model.Registration;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
     List<Registration> findByEventId(Long eventId);
+
     List<Registration> findByUtilisateurEmail(String email);
+
     // Compter combien de gens sont inscrits à un event
     long countByEventId(Long eventId);
 }
